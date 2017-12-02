ae787885ab928064a1b8c2fb3347a11fc8346659
immontilla <mauricio@immontilla.eu>
2017-12-02 16:38:45 +0100

    Client redirection error - Fixed

--- openid-connect-server-webapp/src/main/resources/db/hsql/clients.sql
+++ openid-connect-server-webapp/src/main/resources/db/hsql/clients.sql
@@ -22,8 +22,7 @@ INSERT INTO client_scope_TEMP (owner_id, scope) VALUES
 	('client', 'offline_access');
 
 INSERT INTO client_redirect_uri_TEMP (owner_id, redirect_uri) VALUES
-	('client', 'http://localhost/'),
-	('client', 'http://localhost:8080/');
+	('client', 'http://localhost:8080/simple-web-app/openid_connect_login');
 	
 INSERT INTO client_grant_type_TEMP (owner_id, grant_type) VALUES
 	('client', 'authorization_code'),
